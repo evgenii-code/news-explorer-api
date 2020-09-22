af430d81e0c9bb9583ded2b4898cdc24b3f6da35
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-22 17:07:28 +0300

    Initial app setup

--- app.js
+++ app.js
@@ -3,31 +3,34 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const { errors } = require('celebrate');
-// const { celebrate } = require('celebrate');
+const { errors } = require('celebrate');
+const { celebrate } = require('celebrate');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const users = require('./routes/users.js');
-// const cards = require('./routes/cards.js');
-// const { login, createUser } = require('./controllers/users');
-// const auth = require('./middlewares/auth');
+const users = require('./routes/users.js');
+const articles = require('./routes/articles.js');
+const {
+  login,
+  createUser,
+} = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const errorHandler = require('./middlewares/error');
-// const { loginSchema, createUserSchema, authSchema } = require('./utils/validation-schemes');
+const { loginSchema, createUserSchema, authSchema } = require('./utils/validation-schemes');
 
 require('dotenv').config();
 
 const app = express();
 const {
   PORT = 3000,
-  DB_DOMEN = 'mongodb://localhost:27017/news-explorer-db',
+  DB_DOMAIN = 'mongodb://localhost:27017/news-explorer-db',
 } = process.env;
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 
-mongoose.connect(DB_DOMEN, {
+mongoose.connect(DB_DOMAIN, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -41,18 +44,19 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
-// app.post('/signin', celebrate(loginSchema), login);
-// app.post('/signup', celebrate(createUserSchema), createUser);
-// app.use(celebrate(authSchema), auth);
-// app.use('/users', users);
-// app.use('/cards', cards);
+app.post('/signin', celebrate(loginSchema), login);
+app.post('/signup', celebrate(createUserSchema), createUser);
+app.use(celebrate(authSchema), auth);
+app.use('/users', users);
+app.use('/articles', articles);
+
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
 app.use(errorLogger);
 
-// app.use(errors());
+app.use(errors());
 
 app.use(errorHandler);
 
