0255814ee19ee71f0d577ecdc6052cfd2e106827
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-22 17:01:30 +0300

    authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/auth-err');
+
+require('dotenv').config();
+
+const { NODE_ENV = 'development', JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    return next(new AuthError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+
+  return next();
+};
